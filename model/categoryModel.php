508399504800b8f6ca7164b452600faeecb0c656
Abtahi360 <abtahibinislam2018@gmail.com>
2026-05-12 03:10:57 +0600

    Implement functions to retrieve top and subcategories from the database

--- model/categoryModel.php
+++ model/categoryModel.php
@@ -1,5 +1,27 @@
 <?php
 require_once('../config/db.php');
 
+function getTopCategories() {
+        $con  = getConnection();
+        $sql = mysqli_prepare($con, "SELECT * FROM categories WHERE parent_id IS NULL ORDER BY name ASC");
+        mysqli_stmt_execute($sql);
+        $result = mysqli_stmt_get_result($sql);
+        $rows   = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_close($con);
+        return $rows;
+}
+
+    function getSubCategories($parentId) {
+        $con  = getConnection();
+        $sql = mysqli_prepare($con, "SELECT * FROM categories WHERE parent_id = ? ORDER BY name ASC");
+        mysqli_stmt_bind_param($sql, "i", $parentId);
+        mysqli_stmt_execute($sql);
+        $result = mysqli_stmt_get_result($sql);
+        $rows   = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_close($con);
+        return $rows;
+    }
+
+        
 
 ?>
