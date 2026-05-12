398f70521953a438ee9ac68f18469a379a505a17
Abtahi360 <abtahibinislam2018@gmail.com>
2026-05-12 20:45:06 +0600

    For testing the login page

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-    header('location: ');
+    header('location: views/auth/login.php');
+    // header('location: views/auth/profile.php');
+    // header('location: views/auth/register.php');
+    // header('location: views/member/home.php');
+    // header('location: views/auth/login.php');
+    // header('location: views/auth/login.php');
     exit;
 ?>
