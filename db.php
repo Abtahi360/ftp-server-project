4934909290326afe1638916c357fa89852d7ebb4
abrarArian <sayadulabrar42@gmail.com>
2026-05-13 01:08:06 +0600

    Update db.php

--- db.php
+++ db.php
@@ -6,3 +6,10 @@ $password = "";
 
 try {
     $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $exception) {
+    
+    echo "Connection error: " . $exception->getMessage();
+}
+?>
