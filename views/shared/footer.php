963f1e5bd062076fd1e21d0c151bfac1b34571fb
Abtahi360 <abtahibinislam2018@gmail.com>
2026-05-14 18:24:18 +0600

    Again uploading the userModel besouse  the member of task 2 create a mistake

--- views/shared/footer.php
+++ views/shared/footer.php
@@ -6,7 +6,7 @@
 </div>
 
 <footer class="site-footer">
-    <p>&copy; <?= date('Y') ?> FTP Media System - ISP Content Portal &nbsp;|&nbsp; Web Technologies Project</p>
+    <p>&copy; <?= date('Y') ?> FTP Media System - ISP Content Portal</p>
 </footer>
 
 </body>
