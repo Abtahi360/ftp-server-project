1f41b7523f7c1082c8ff8adae6f308f4a8dffdbd
Abtahi360 <abtahibinislam2018@gmail.com>
2026-05-18 02:13:24 +0600

    Finding some issue, thast why not run our project. Fix thus bit issue..!

--- models/userModel.php
+++ models/userModel.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('/../config/db.php');
+    require_once(__DIR__ . '/../config/db.php');
     function getUserByEmail($email) {
         $con = getConnection();
         $stmt = mysqli_prepare($con, "SELECT * FROM users WHERE email = ? LIMIT 1");
